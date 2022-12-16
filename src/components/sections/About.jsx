2f0b383074f7176efdefa456c93a7ac1e02f3cd9
Chris <condreaychris@gmail.com>
2022-12-16 10:07:41 -0800

    working on improvements

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -6,22 +6,19 @@ function About() {
       <div className="container">
         <div className="about">
           <h2 className="title">About Me</h2>
-          <div className="content">
-            <div className="row">
-              <div className="col-md-3">
-                <div className="text-center text-md-left">
-                  <img className="profile-photo" src="/images/profile.jpg" alt="" /></div>
-                </div>
-              {/* <div className="spacer d-md-none d-lg-none"></div> */}
-              <div className="col-md-9 text-md-left">
-                <p className="introduction">My name is Chris and I am a Fullstack Web Developer. I attended an online course at UCI and achieved a certificate for coding. I create clean, polished websites and continuing my knowledge and pursuit towards more coding.</p>
-                <div>
-                  <a href="/images/Condreay_Christopher.pdf" target={'_blank'}>
-                    <button className="btn resume-btn border-round">
-                      Download Resume
-                    </button>
-                  </a>
-                </div>
+          <div className="row">
+            <div className="col-md-3">
+              <div className="text-center">
+                <img className="profile-photo" src="/images/profile.jpg" alt="" /></div>
+              </div>
+            <div className="col-md-9 text-md-left">
+              <p className="introduction">My name is Chris and I am a Fullstack Web Developer. I attended an online course at UCI and achieved a certificate for coding. I create clean, polished websites and continuing my knowledge and pursuit towards more coding.</p>
+              <div>
+                <a href="/images/Condreay_Christopher.pdf" target={'_blank'}>
+                  <button className="btn resume-btn border-round">
+                    Download Resume
+                  </button>
+                </a>
               </div>
             </div>
           </div>
