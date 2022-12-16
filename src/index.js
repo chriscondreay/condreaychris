36998160ca7dce27711f2df179605028efb414de
Chris <condreaychris@gmail.com>
2022-12-16 11:20:04 -0800

    changes with css and js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/scss/bootstrap.scss';
-import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'font-awesome/css/font-awesome.min.css';
 import App from './App';
 
